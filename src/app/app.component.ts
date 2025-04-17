4cc27c38565232eebdaf8b6188d74e3978584eb2
CleiverMejia <cleivermejiar@gmail.com>
2025-04-17 19:54:58 +0000

    Fifth commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
+
 import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { FcmService } from '@services/fcm/fcm.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  standalone: false,
+  standalone: false
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private fcmService: FcmService
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.fcmService.initPush();
+    });
+  }
 }
